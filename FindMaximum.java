a16f24c3038f5b6a2778e9a597b8b408c23defc7
pratikshabathe <pratikshabathe11@gmail.com>
2023-01-20 21:35:32 +0530

    Added: Find the maximum string

--- FindMaximum.java
+++ FindMaximum.java
@@ -11,8 +11,9 @@ public class FindMaximum {
 	this.r = r;
 	}
 	
-	public static Float findMaximum(Float p , Float q, Float r) {
-		Float max = p;              //Initializing p as greater
+	public static String findMaximum(String p , String q, String r) {
+		
+		String max = p;              //Initializing p as greater
 		if(q.compareTo(max) > 0)
 			max = q;                 // now q is greater
 		if(r.compareTo(max) > 0)
@@ -22,15 +23,16 @@ public class FindMaximum {
 		return max;
 	}
 
-	private static void printMax(Float p, Float q, Float r, Float max) {
+	private static void printMax(String p,String q, String r, String max) {
 		
 		System.out.printf("the max of value is ", p, q, r, max);
 		
 	}
 	
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to find Maximum problem using Generics \n ");
-		System.out.println("The maximum value between the three integer is " +findMaximum(3.3f, 8.2f, 5.7f));
+		System.out.println("The maximum value between the three String is " +findMaximum("Riya", "Shubham", "Avni"));
 		
 	}
 }

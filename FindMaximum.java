8f6a0dbe5fe81abe48a1fbc9df87c507938cc1b5
pratikshabathe <pratikshabathe11@gmail.com>
2023-01-20 20:44:44 +0530

    Added: Find The Maximum Float

--- FindMaximum.java
+++ FindMaximum.java
@@ -11,9 +11,8 @@ public class FindMaximum {
 	this.r = r;
 	}
 	
-	public static Integer findMaximum(Integer p , Integer q, Integer r) {
-		
-		Integer max = p;              //Initializing p as greater
+	public static Float findMaximum(Float p , Float q, Float r) {
+		Float max = p;              //Initializing p as greater
 		if(q.compareTo(max) > 0)
 			max = q;                 // now q is greater
 		if(r.compareTo(max) > 0)
@@ -23,15 +22,15 @@ public class FindMaximum {
 		return max;
 	}
 
-	private static void printMax(Integer p, Integer q, Integer r, Integer max) {
-		// TODO Auto-generated method stub
+	private static void printMax(Float p, Float q, Float r, Float max) {
+		
 		System.out.printf("the max of value is ", p, q, r, max);
 		
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to find Maximum problem using Generics \n ");
-		System.out.println("The maximum value between the three integer is " +findMaximum(3, 8, 5));
+		System.out.println("The maximum value between the three integer is " +findMaximum(3.3f, 8.2f, 5.7f));
 		
 	}
 }

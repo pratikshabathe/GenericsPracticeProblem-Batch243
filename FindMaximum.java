3f19f9465087bc010916c31fdb4daf4e215b86fa
pratikshabathe <pratikshabathe11@gmail.com>
2023-01-20 21:50:01 +0530

    Added: Refactor all the 3 to One generic method

--- FindMaximum.java
+++ FindMaximum.java
@@ -11,9 +11,9 @@ public class FindMaximum {
 	this.r = r;
 	}
 	
-	public static String findMaximum(String p , String q, String r) {
+	public static <T extends Comparable <T>> T findMaximum(T p , T q, T r) {
 		
-		String max = p;              //Initializing p as greater
+		T max = p;              //Initializing p as greater
 		if(q.compareTo(max) > 0)
 			max = q;                 // now q is greater
 		if(r.compareTo(max) > 0)
@@ -23,7 +23,7 @@ public class FindMaximum {
 		return max;
 	}
 
-	private static void printMax(String p,String q, String r, String max) {
+	private static <T> void printMax(T p , T q, T r, T max) {
 		
 		System.out.printf("the max of value is ", p, q, r, max);
 		
@@ -32,7 +32,9 @@ public class FindMaximum {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to find Maximum problem using Generics \n ");
-		System.out.println("The maximum value between the three String is " +findMaximum("Riya", "Shubham", "Avni"));
+		System.out.println("The maximum value between the three integer is " +findMaximum(3, 8, 5) + "\n");
+		System.out.println("The maximum value between the three float is " +findMaximum(1.5f, 3.56f, 5.87f) + "\n");
+		System.out.println("The maximum value between the three String is " +findMaximum("abc", "pqr", "xyz") + "\n");
 		
 	}
 }
